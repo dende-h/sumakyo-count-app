a2ed15f9a490aeecaf9f67a0b9d391e8dbceb69e
dende-h <miyuki_dende@yahoo.co.jp>
2022-03-24 09:35:23 +0900

    入力フォーム作成

---
+++ src/components/useCountUpDown.tsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+
+export const useCountUpDown = () => {
+	const [count, setCount] = useState<number>(0);
+
+	const upButtonClick = () => {
+		setCount(count + 1);
+	};
+	const downButtonClick = () => {
+		setCount(count - 1);
+	};
+
+	return { count, upButtonClick, downButtonClick };
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,56 +1,123 @@
-import {
-  Link as ChakraLink,
-  Text,
-  Code,
-  List,
-  ListIcon,
-  ListItem,
-} from '@chakra-ui/react'
-import { CheckCircleIcon, LinkIcon } from '@chakra-ui/icons'
+import { Box, Button, Divider, Input, Select, Stack, Text } from "@chakra-ui/react";
+import { createClient, SupabaseClient } from "@supabase/supabase-js";
+import { useState } from "react";
+import format from "date-fns/format";
+import { useCountUpDown } from "../components/useCountUpDown";
+import { CustomDatePickerCalendar } from "../components/CustomDatePickerCalendar";
+import { useRecoilValue } from "recoil";
+import { dateState } from "../globalState/dateState";
 
-import { Hero } from '../components/Hero'
-import { Container } from '../components/Container'
-import { Main } from '../components/Main'
-import { DarkModeSwitch } from '../components/DarkModeSwitch'
-import { CTA } from '../components/CTA'
-import { Footer } from '../components/Footer'
+type yearMonth = {
+	id?: number;
+	created_at?: string;
+	year_month: string;
+	user_id?: string;
+	shop_name: string;
+};
 
-const Index = () => (
-  <Container height="100vh">
-    <Hero />
-    <Main>
-      <Text>
-        Example repository of <Code>Next.js</Code> + <Code>chakra-ui</Code> +{' '}
-        <Code>TypeScript</Code>.
-      </Text>
+type userCount = {
+	id?: number;
+	created_at?: string;
+	u_usercount: number;
+	new_usercount: number;
+	seminar_count: number;
+	mx_seminar_count: number;
+	mx_usercount: number;
+	user_id?: number;
+	year_month_id?: number;
+};
 
-      <List spacing={3} my={0}>
-        <ListItem>
-          <ListIcon as={CheckCircleIcon} color="green.500" />
-          <ChakraLink
-            isExternal
-            href="https://chakra-ui.com"
-            flexGrow={1}
-            mr={2}
-          >
-            Chakra UI <LinkIcon />
-          </ChakraLink>
-        </ListItem>
-        <ListItem>
-          <ListIcon as={CheckCircleIcon} color="green.500" />
-          <ChakraLink isExternal href="https://nextjs.org" flexGrow={1} mr={2}>
-            Next.js <LinkIcon />
-          </ChakraLink>
-        </ListItem>
-      </List>
-    </Main>
+const supabase: SupabaseClient = createClient(
+	process.env.NEXT_PUBLIC_SUPABASE_URL,
+	process.env.NEXT_PUBLIC_SUPABASE_KEY
+);
 
-    <DarkModeSwitch />
-    <Footer>
-      <Text>Next ❤️ Chakra</Text>
-    </Footer>
-    <CTA />
-  </Container>
-)
+const Index = ({ year_month }) => {
+	const YearMonth: yearMonth[] = [...year_month];
+	console.log(YearMonth);
 
-export default Index
+	const selectYearMonthList = YearMonth.map((item) => {
+		return item.year_month;
+	});
+
+	const shopNameList = YearMonth.map((item) => {
+		return item.shop_name;
+	});
+	console.log(shopNameList);
+	const shopName = shopNameList[0];
+
+	const [achievement, setAchievement] = useState<userCount>();
+	const inputDate = useRecoilValue(dateState);
+	const seminarCount = useCountUpDown();
+	const uniqueUserCount = useCountUpDown();
+	const newUserCount = useCountUpDown();
+	const mxSeminarCount = useCountUpDown();
+	const mxUserCount = useCountUpDown();
+	console.log(inputDate);
+
+	const onSubmit = async () => {
+		setAchievement({
+			...achievement,
+			u_usercount: uniqueUserCount.count,
+			new_usercount: newUserCount.count,
+			seminar_count: seminarCount.count,
+			mx_seminar_count: mxSeminarCount.count,
+			mx_usercount: mxUserCount.count
+		});
+		console.log(achievement);
+		await supabase.from("achievements").insert(achievement);
+	};
+
+	return (
+		<>
+			<Box>
+				<Stack>
+					<Text>{`${shopName}スマホ教室実績入力フォーム`}</Text>
+					<Divider />
+					<Box>
+						<CustomDatePickerCalendar />
+					</Box>
+					<Box>
+						<Text>講座開催数</Text>
+						<Text>{seminarCount.count}</Text>
+						<Button onClick={seminarCount.upButtonClick}>Up</Button>
+						<Button onClick={seminarCount.downButtonClick}>down</Button>
+					</Box>
+					<Box>
+						<Text>ユニークユーザー数</Text>
+						<Text>{uniqueUserCount.count}</Text>
+						<Button onClick={uniqueUserCount.upButtonClick}>Up</Button>
+						<Button onClick={uniqueUserCount.downButtonClick}>down</Button>
+					</Box>
+					<Box>
+						<Text>新規ユーザー数</Text>
+						<Text>{newUserCount.count}</Text>
+						<Button onClick={newUserCount.upButtonClick}>Up</Button>
+						<Button onClick={newUserCount.downButtonClick}>down</Button>
+					</Box>
+					<Box>
+						<Text>MX講座開催数</Text>
+						<Text>{mxSeminarCount.count}</Text>
+						<Button onClick={mxSeminarCount.upButtonClick}>Up</Button>
+						<Button onClick={mxSeminarCount.downButtonClick}>down</Button>
+					</Box>
+					<Box>
+						<Text>MX講座ユーザー数</Text>
+						<Text>{mxUserCount.count}</Text>
+						<Button onClick={mxUserCount.upButtonClick}>Up</Button>
+						<Button onClick={mxUserCount.downButtonClick}>down</Button>
+					</Box>
+					<Box>
+						<Button onClick={onSubmit}>送信</Button>
+					</Box>
+				</Stack>
+			</Box>
+		</>
+	);
+};
+
+export const getServerSideProps = async () => {
+	const { data: year_month, error } = await supabase.from("year_month").select("*");
+	return { props: { year_month } };
+};
+export default Index;
