50d35b1b08d98802924d54fce5a43560d7f3676c
dende-h <miyuki_dende@yahoo.co.jp>
2022-04-13 07:57:11 +0900

    progressCard作成

---
+++ src/components/ProgressCard.tsx
@@ -0,0 +1,64 @@
+import { Divider, Stack, Text } from "@chakra-ui/react";
+import { memo } from "react";
+import { goal } from "../pages/goal_setting";
+
+type Props = {
+	label: string;
+	total: number;
+	goal: goal;
+};
+
+export const ProgressCard = memo((props: Props) => {
+	const { label, total, goal } = props;
+
+	console.log(label);
+	const progressCalc = () => {
+		switch (label) {
+			case "講座開催数":
+				//残り件数
+				const remainingSeminar = goal.seminar_goal - total;
+				//進捗率
+				const rateOfProgressSeminar = total / goal.seminar_goal;
+
+				return { goal: goal.seminar_goal, remaining: remainingSeminar, rateOfProgress: rateOfProgressSeminar };
+			case "リピートユーザー数":
+				//残り件数
+				const remainingUniqueUser = goal.u_user_goal - total;
+				//進捗率
+				const rateOfProgressUniqueUser = total / goal.u_user_goal;
+
+				return { goal: goal.seminar_goal, remaining: remainingUniqueUser, rateOfProgress: rateOfProgressUniqueUser };
+			case "新規ユーザー数":
+				//残り件数
+				const remainingNewUser = goal.new_user_goal - total;
+				//進捗率
+				const rateOfProgressNewUser = total / goal.new_user_goal;
+
+				return { goal: goal.seminar_goal, remaining: remainingNewUser, rateOfProgress: rateOfProgressNewUser };
+			default:
+				return null;
+		}
+	};
+
+	const progressCardContent = progressCalc();
+	console.log(progressCardContent);
+
+	return (
+		<>
+			{progressCardContent ? (
+				<Stack p={4} w={"250px"} backgroundColor={"twitter.100"} textAlign="center" borderRadius={"md"}>
+					<Text fontSize={"lg"} fontWeight="bold">
+						目標{label}
+					</Text>
+					<Divider borderColor={"blue.500"} />
+					<Text fontSize={"x-large"} fontWeight={"bold"}>
+						現在の実績{total}/ 目標値{progressCardContent.goal}/ 残り{progressCardContent.remaining} 件/
+						{new Intl.NumberFormat("ja", { style: "percent", maximumSignificantDigits: 3 }).format(
+							progressCardContent.rateOfProgress
+						)}
+					</Text>
+				</Stack>
+			) : undefined}
+		</>
+	);
+});
--- src/pages/progress.tsx
+++ src/pages/progress.tsx
@@ -1,4 +1,131 @@
-const Progress = () => {
-	return null;
+import { createClient, SupabaseClient } from "@supabase/supabase-js";
+import { useYearMonthDataSet } from "../hooks/useYearMonthDataSet";
+import { useAchievementDataSet } from "../hooks/useAchievementDataSet";
+import {
+	Box,
+	Select,
+	Stack,
+	Text,
+	Button,
+	Wrap,
+	WrapItem,
+	Table,
+	Thead,
+	Tbody,
+	Tfoot,
+	Tr,
+	Th,
+	Td,
+	TableCaption,
+	HStack
+} from "@chakra-ui/react";
+import { useRecoilState, useRecoilValue } from "recoil";
+import { goalValueState } from "../globalState/goalValueState";
+import { useEffect } from "react";
+import { ProgressCard } from "../components/ProgressCard";
+import { onSelectedShopName } from "../globalState/onSelectedShopName";
+import { onSelectYearMonthState } from "../globalState/onSelectYearMonthState";
+import { isNotEmptyObject } from "@chakra-ui/utils";
+
+//supabaseのAPI定義
+const supabase: SupabaseClient = createClient(
+	process.env.NEXT_PUBLIC_SUPABASE_URL,
+	process.env.NEXT_PUBLIC_SUPABASE_KEY
+);
+
+const Progress = ({ year_month, achievements, goalValue }) => {
+	const { inputShopName, selectOption, selectShopName, selectYearMonth, shopNameList } = useYearMonthDataSet({
+		year_month
+	});
+
+	const { totalAchievements, showAchievements } = useAchievementDataSet({ achievements });
+
+	const [goal, setGoal] = useRecoilState(goalValueState);
+
+	useEffect(() => {
+		setGoal(goalValue);
+	}, []);
+
+	console.log(goal);
+
+	const onSelectShopName = useRecoilValue(onSelectedShopName);
+	const onSelectYearMonth = useRecoilValue(onSelectYearMonthState);
+
+	//選択された店舗と年月で目標をfilter
+	const selectedGoalValueArray = goal.filter((item) => {
+		return item.shop_name_month.includes(onSelectShopName) && item.shop_name_month.includes(onSelectYearMonth);
+	});
+
+	//オブジェクトに変換
+	const selectedGoalValueObjectArray = { ...selectedGoalValueArray };
+	const selectedGoalValueObject = selectedGoalValueObjectArray[0];
+	console.log(selectedGoalValueObject);
+
+	//現在の進捗率
+
+	return (
+		<>
+			<Text fontSize={"lg"} fontWeight={"bold"} marginLeft={4} marginTop={4}>
+				店舗と年月を選択
+			</Text>
+			<Stack p={2}>
+				<Select
+					onChange={selectYearMonth.onChangeSelect}
+					placeholder={"年月を選択"}
+					defaultValue={""}
+					backgroundColor={"white"}
+				>
+					{selectOption.map((item) => {
+						return (
+							<option key={item} value={`${item}`}>
+								{item}
+							</option>
+						);
+					})}
+				</Select>
+				<Select
+					onChange={selectShopName.onChangeSelect}
+					placeholder={"店舗を選択"}
+					defaultValue={""}
+					backgroundColor={"white"}
+				>
+					{shopNameList.map((item) => {
+						return (
+							<option key={item} value={`${item}`}>
+								{item}
+							</option>
+						);
+					})}
+				</Select>
+			</Stack>
+			<Wrap>
+				{totalAchievements.map((item) => {
+					return (
+						<WrapItem key={item.label}>
+							<ProgressCard label={item.label} total={item.total} goal={selectedGoalValueObject} />
+						</WrapItem>
+					);
+				})}
+			</Wrap>
+		</>
+	);
 };
+
+export const getStaticProps = async () => {
+	const { data: year_month, error: year_monthError } = await supabase.from("year_month").select("*");
+	if (year_monthError) {
+	}
+	const { data: achievements, error: achievementsError } = await supabase
+		.from("achievements")
+		.select("*")
+		.order("date_of_results", { ascending: true });
+	if (achievementsError) {
+	}
+	const { data: goalValue, error: goalValueError } = await supabase.from("goal_value").select("*");
+
+	if (goalValueError) {
+	}
+	return { props: { year_month, achievements, goalValue }, revalidate: 10 };
+};
+
 export default Progress;
