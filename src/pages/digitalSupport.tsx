12e2b6a472715664b7beea1c15ca37bf11b779ad
dende-h <miyuki_dende@yahoo.co.jp>
2022-05-18 02:41:09 +0900

    tabによるfilter機能のバグ修正

--- src/pages/digitalSupport.tsx
+++ src/pages/digitalSupport.tsx
@@ -14,13 +14,15 @@ import {
 	useColorModeValue
 } from "@chakra-ui/react";
 import { createClient, SupabaseClient } from "@supabase/supabase-js";
-import { useEffect, useState } from "react";
-import { useRecoilValue, useSetRecoilState } from "recoil";
+import { useEffect } from "react";
+import { useRecoilState, useRecoilValue, useSetRecoilState } from "recoil";
 import { DigitalSupportInputModal } from "../components/DigitalSupportInputModal";
 import { digitalSupportEventArray } from "../globalState/digitalSupportEventArray";
-import { digitalSupport, digitalSupportState } from "../globalState/digitalSupportState";
+import { digitalSupportState } from "../globalState/digitalSupportState";
 import { shopNameArray } from "../globalState/shopNameArray";
 import { ShowDigitalSupportCard } from "../components/ShowDigitalSupportCard";
+import { showDigitalSupportAchievements } from "../globalState/selector/showDigitalSupportAchievements";
+import { tabIndexState } from "../globalState/tabIndexState";
 
 //supabaseのAPI定義
 const supabase: SupabaseClient = createClient(
@@ -42,30 +44,19 @@ const DigitalSupport = ({ digital_support }) => {
 	//タブのカラー設定
 	const tabColors = useColorModeValue(["teal.50", "blue.100", "orange.100"], ["teal.900", "twitter.900", "orange.900"]);
 	//選択しているタブのindexナンバー
-	const [tabIndex, setTabIndex] = useState(enzanShop);
+	const [tabIndex, setTabIndex] = useRecoilState(tabIndexState);
 	//indexによって背景色変更
 	const tabBgColor = tabColors[tabIndex];
 
 	const tabNameArray = useRecoilValue(shopNameArray).filter((item) => item !== "全店舗");
 
 	//選択しているタブによってdigital_supportをfilterする
-	const initialdata = useRecoilValue(digitalSupportState);
-	const initialDigitalSupport: digitalSupport[] = initialdata.filter((item) => {
-		return item.shop_name === tabNameArray[enzanShop];
-	});
+	const filterData = useRecoilValue(showDigitalSupportAchievements);
 
-	const [digitalSupportArray, setDigitalSupportArray] = useState(initialDigitalSupport);
-
-	useEffect(() => {
-		const newShowDigitalSupportArray = initialdata.filter((item) => {
-			return item.shop_name === tabNameArray[tabIndex];
-		});
-		setDigitalSupportArray(newShowDigitalSupportArray);
-	}, [tabIndex]);
 	//指定5講座が実績にあるかどうか
 	const eventNameArray = useRecoilValue(digitalSupportEventArray);
 	const eventImplementation = eventNameArray.map((item) => {
-		return digitalSupportArray.map((item) => item.event_name).includes(item);
+		return filterData.map((item) => item.event_name).includes(item);
 	});
 
 	return (
@@ -80,10 +71,10 @@ const DigitalSupport = ({ digital_support }) => {
 					<Center>
 						<Wrap fontSize={["large", "x-large"]} fontWeight={"bold"} m={2} spacing={[5, 8, 10]}>
 							<WrapItem>
-								<Text>デジ活教室実施回数：{digitalSupportArray.length}回</Text>
+								<Text>デジ活教室実施回数：{filterData.length}回</Text>
 							</WrapItem>
 							<WrapItem>
-								<Text>Min目標まで残り{digitalSupportGoalSetting - digitalSupportArray.length}回</Text>
+								<Text>Min目標まで残り{digitalSupportGoalSetting - filterData.length}回</Text>
 							</WrapItem>
 							<WrapItem>
 								<Text>指定講座実施状況：現在{eventImplementation.filter((item) => item === true).length}講座達成</Text>
@@ -108,7 +99,7 @@ const DigitalSupport = ({ digital_support }) => {
 						<TabList>
 							{tabNameArray.map((item, index) => {
 								return (
-									<Tab bg={tabIndex === index ? tabBgColor : "gray.100"} fontSize={["sm", "md", "large"]}>
+									<Tab key={index} bg={tabIndex === index ? tabBgColor : "gray.100"} fontSize={["sm", "md", "large"]}>
 										{item}
 									</Tab>
 								);
@@ -116,13 +107,13 @@ const DigitalSupport = ({ digital_support }) => {
 						</TabList>
 						<TabPanels bg={tabBgColor}>
 							<TabPanel>
-								<ShowDigitalSupportCard showDigitalSupportArray={digitalSupportArray} />
+								<ShowDigitalSupportCard showDigitalSupportArray={filterData} />
 							</TabPanel>
 							<TabPanel>
-								<ShowDigitalSupportCard showDigitalSupportArray={digitalSupportArray} />
+								<ShowDigitalSupportCard showDigitalSupportArray={filterData} />
 							</TabPanel>
 							<TabPanel>
-								<ShowDigitalSupportCard showDigitalSupportArray={digitalSupportArray} />
+								<ShowDigitalSupportCard showDigitalSupportArray={filterData} />
 							</TabPanel>
 						</TabPanels>
 					</Tabs>
